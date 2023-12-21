356c7aaa6616ef4b04ccf946aa4fddc57e1fcea7
Maximilian Welsch <maxwelsch93@gmail.com>
2023-12-21 23:13:46 +0100

    add some more methods for implementing queues, deques and stacks

--- ds/linkedlist.go
+++ ds/linkedlist.go
@@ -34,7 +34,7 @@ func (ll *LinkedList[T]) AddLast(t T) {
     ll.addBefore(t, ll.sentinel)
 }
 
-
+// Clear removes all values from the list and reinitializes it.
 func (ll *LinkedList[T]) Clear() {
     ll.init()
     ll.size = 0
@@ -42,12 +42,12 @@ func (ll *LinkedList[T]) Clear() {
 
 // Contains checks if the given element is present within the list.
 func (ll *LinkedList[T]) Contains(t T) bool {
-    _, ok := ll.GetIndexOf(t)
+    _, ok := ll.IndexOf(t)
     return ok
 }
 
 // Get returns the value of the element stored at the given index. If the
-// given index is negative or >= the list size, a zero value and an error
+// index is out of bounds (index < 0 || index >= Size()), a non-nill error 
 // is returned.
 func (ll *LinkedList[T]) Get(index int) (T, error) {
     x, err := ll.getNodeAt(index)
@@ -60,11 +60,22 @@ func (ll *LinkedList[T]) Get(index int) (T, error) {
     return x.val, nil
 }
 
-// GetIndexOf returns the index in this list of the first occurrence of the
-// specified element. Succes is indicated by the second return value being
-// true. If the element is not in the list the index is return as -1 and
-// the second return value is false.
-func (ll *LinkedList[T]) GetIndexOf(t T) (int, bool) {
+// GetFirst returns the first element from the linked list. If the list is empty
+// a non-nil error is returned
+func (ll *LinkedList[T]) GetFirst() (T, error) {
+
+    if ll.IsEmpty() {
+        var t T
+        return t, errors.New("cannot return element from empty list")
+    }
+
+    return ll.sentinel.next.val, nil
+}
+
+// IndexOf returns the index  of the first occurrence of the specified element.
+// Succes is indicated by the second return value being true. If the element is not 
+// in the list the index is return as -1 and the second return value is false.
+func (ll *LinkedList[T]) IndexOf(t T) (int, bool) {
     x := ll.sentinel.next
     var index int
 
@@ -78,19 +89,6 @@ func (ll *LinkedList[T]) GetIndexOf(t T) (int, bool) {
     }
 
     return -1, false
-}
-
-
-// GetFirst returns the first element from the linked list. If the list is empty
-// a non-nil error is returned
-func (ll *LinkedList[T]) GetFirst() (T, error) {
-
-    if ll.IsEmpty() {
-        var t T
-        return t, errors.New("cannot return element from empty list")
-    }
-
-    return ll.sentinel.next.val, nil
 }
 
 // GetLast returns the last element from the linked list. If the list is empty
@@ -108,6 +106,113 @@ func (ll *LinkedList[T]) GetLast() (T, error) {
 // IsEmpty checks if the list contains at least one element.
 func (ll *LinkedList[T]) IsEmpty() bool {
 	return ll.sentinel.next == ll.sentinel && ll.sentinel.prev == ll.sentinel
+}
+
+// Offer adds the given element at the end of list. Returns true to indicate
+// succes as specified by Queue interface
+func (ll *LinkedList[T]) Offer(t T)  bool {
+     return ll.OfferLast(t)
+}
+
+// OfferFirst adds the given element at the beginning of the list. Returns true 
+// to indicate succes as specified by Dequeue interface.
+func (ll *LinkedList[T]) OfferFirst(t T) bool {
+    ll.AddFirst(t)
+    return true
+}
+
+// OfferLast adds the given element at the end of the list. Returns true 
+// to indicate succes as specified by Dequeue interface.
+func (ll *LinkedList[T]) OfferLast(t T) bool {
+    ll.Add(t)
+    return true
+}
+// Peek retrieves, but does not remove the first element of the queue.
+// If the list is not empty, an additional true flag is returned, otherwise 
+// a zero value and a false flag are returned.
+func (ll *LinkedList[T]) Peek() (T, bool) {
+    return ll.PeekFirst()
+}
+
+// PeekFirst retrieves, but does not remove the first element of the queue.
+// If the list is not empty, an additional true flag is returned, otherwise 
+// a zero value and a false flag are returned.
+func (ll *LinkedList[T]) PeekFirst() (T, bool) {
+
+    x, err := ll.GetFirst()
+
+    if err != nil {
+        var r T
+        return r, false
+    }
+
+    return x, true
+}
+
+// PeekLast retrieves, but does not remove the last element of the queue.
+// If the list is not empty, an additional true flag is returned, otherwise 
+// a zero value and a false flag are returned.
+func (ll *LinkedList[T]) PeekLast() (T, bool) {
+
+    x, err := ll.GetLast()
+
+    if err != nil {
+        var r T
+        return r, false
+    }
+
+    return x, true
+}
+
+// Push pushes an element onto the stack represented by this list. In other
+// words, inserts it at the beginning of this list. This method is equivalent
+// to AddFirst
+func (ll *LinkedList[T]) Push(t T) {
+    ll.AddFirst(t)
+}
+
+// Pop pops an element from the stack represented by this list. In other
+// words, removes and returns the first element of this list. This method
+// is equivalent to RemoveFirst
+func (ll *LinkedList[T]) Pop() (T, error) {
+    return ll.RemoveFirst()
+}
+
+// Poll retrieves and removes the first element of the list. If the list
+// is not empty, an additional true flag is returned, otherwise a zero value
+// and a false flag are returned.
+func (ll *LinkedList[T]) Poll() (T, bool) {
+    return ll.PollFirst()
+}
+
+// PollFirst retrieves and removes the first element of the list. If the list
+// is not empty, an additional true flag is returned, otherwise a zero value
+// and a false flag are returned.
+func (ll *LinkedList[T]) PollFirst() (T, bool) {
+
+    x, err := ll.RemoveFirst()
+
+    if err != nil {
+        var r T
+        return r, false
+    }
+
+    return x, true
+}
+
+// PollFirst retrieves and removes the first element of the list. If the list
+// is not empty, an additional true flag is returned, otherwise a zero value
+// and a false flag are returned.
+func (ll *LinkedList[T]) PollLast() (T, bool) {
+
+    x, err := ll.RemoveLast()
+
+    if err != nil {
+        var r T
+        return r, false
+    }
+
+    return x, true
 }
 
 // Remove removes the first ListNode storing the given element from the list and returns true.
@@ -150,6 +255,22 @@ func (ll *LinkedList[T]) RemoveAt(index int) (T, error) {
     }
 
     return ll.removeNode(node)
+}
+
+// Set replaces the value of the element stored at the given index and returns the old value.
+// If the index is out of bounds, an non-nil error is returned.
+func (ll *LinkedList[T]) Set(index int, t T) (T, error) {
+    x, err := ll.getNodeAt(index)
+
+    var r T
+    if err != nil {
+        return r, err
+    }
+
+    r = x.val
+    x.val = t
+    
+    return  r, nil
 }
 
 // Size returns the current number of elements in the list.
@@ -244,7 +365,7 @@ func (ll *LinkedList[T]) findNode(t T) *listNode[T] {
 	return nil
 }
 
-// NewLinkedList creates a new and empty LinkedList.
+// NewLinkedList creates a new initialized LinkedList.
 func NewLinkedList[T comparable]() LinkedList[T] {
 	ll := LinkedList[T]{}
     ll.init()
